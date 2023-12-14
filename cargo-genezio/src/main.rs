66b7f70f3502d50db97961ac5fc238b0c426fcef
Laurentiu Ciobanu <laurentiu@ciobanu.dev>
2023-12-14 18:32:35 +0200

    fix: proper app name

--- cargo-genezio/src/main.rs
+++ cargo-genezio/src/main.rs
@@ -9,7 +9,7 @@ mod metadata;
 mod options;
 
 #[derive(Debug, Parser)]
-#[clap(name = "cargo genezio", version)]
+#[clap(name = "cargo genezio", version, bin_name = "cargo")]
 pub struct App {
     #[clap(flatten)]
     global_opts: GlobalOptions,
